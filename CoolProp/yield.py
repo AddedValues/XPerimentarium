9ce910deba6a18af7845c969963ade0c407734ca
Mogens B. Laursen <mbl@addedvalues.eu>
2025-03-04 10:07:54 +0100

    Opdateret VP-performance beregninger med polynom. regression

--- CoolProp/yield.py
+++ CoolProp/yield.py
@@ -20,12 +20,17 @@ Author: Mogens Bech Laursen
 """
 
 import os
+from weakref import ref
 import numpy as np
 import pandas as pd
 # from pyfluids import Fluid, FluidsList
 from CoolProp.CoolProp import PropsSI
 import matplotlib.pyplot as plt
 import xlwings as xw
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+
 
 os.chdir(os.path.join(os.getcwd(), 'CoolProp'))
 print(f'curdir: {os.getcwd()}')
@@ -99,6 +104,42 @@ def calc(TsourceC: float, TdrainC: float, pCond: float, refrig: str) -> list[flo
             'Work': work, 'qEvap': qEvap, 'qOut': work+qEvap, 'Yield': y, 'COP': COP}
     return result
 
+def performPolyFit(degree: int, df: pd.DataFrame, featureNames: list[str]) -> tuple[float, np.ndarray, np.ndarray, LinearRegression]:
+    """ \
+    Perform polynomial regression on the yield data.
+    - degree: Degree of the polynomial regression. Defaults to 2.
+    - dfResults: DataFrame with the results.    
+    - featureNames: List of feature names e.g. ['Tsource', 'Tdrain']
+    Returns:
+    - score: R^2 score.
+    - intercept: Intercept of the polynomial regression.
+    - coef: Coefficients of the polynomial regression for each polynomial combination of features.
+    """
+
+    # TODO Include Tcond / pCond as independent variable.
+    
+    # Reshape the data as a 2D array of independent variable values.
+    X = df[featureNames].values
+    y = df['YieldNormed'].values
+
+    # Perform polynomial regression.
+    poly = PolynomialFeatures(degree=degree)
+    X_poly = poly.fit_transform(X)  # Returns a Vandermonde matrix with the polynomial combinations of the features, one row per observation.
+    model = LinearRegression()
+    linReg = model.fit(X_poly, y)
+
+    # Test the polynomial regression.
+    predictedYield = model.predict(X_poly)
+    
+    # Get the attributes of the model.
+    coef = model.coef_
+    intercept = model.intercept_
+    score = model.score(X_poly, y)
+    print(f'coef: {coef}, intercept: {intercept}, score: {score}')
+
+    return (score, intercept, coef, model)
+
+
 TcondsC = [60.0, 70.0, 80.0]     # Forward condenser temperature in Celsius for NH3.
 pConds  = [90E5, 100E5, 110E5]   # Forward condenser pressure in Pa for CO2.
 TsourceRefC =  4.0               # Independent of refrigerant, tied to source inlet temperature.
@@ -106,8 +147,13 @@ TdrainRefC  = 40.0               # Independent of refrigerant, tied to district 
 
 results: dict[str, pd.DataFrame] = {}  # Key is combination of refrigerant and condenser state (temp. or pressure).
 
+# # DEBUG
+# refrigerants = ['NH3']
+# TcondsC = [60.0]
+# pConds  = [90E5]
+
 for refrig in refrigerants:
-    for iCond in range(3):  # Loops either TcondsC or Pconds
+    for iCond in range(len(TcondsC)):  # Loops either TcondsC or Pconds
 
         dfResults = pd.DataFrame(columns=['Tsource', 'Tdrain', 'pCond', 'pA', 'hA', 'hB', 'hC', 'hD', 'sB', 'Work', 'qEvap', 'qOut', 'Yield', 'COP'])
 
@@ -141,26 +187,44 @@ for refrig in refrigerants:
 
         results[title] = (dfResults, dfYield, dfCOP)
 
-# Write the results to Excel files.
-xlApp = xw.App(visible=True, add_book=True)
-wb = xlApp.books.active
+# Perform polynomial regression on the yield data.
+# See: https://www.kaggle.com/code/peymankarimi74/simple-multiple-and-polynomial-regression
+# See: https://saturncloud.io/blog/multivariate-polynomial-regression-with-python/
+
+regrs: dict[str, tuple[float, np.ndarray, np.ndarray, LinearRegression]] = {}  # Key is title, Value is tuple of score, intercept, coef, model.
+degree = 2
 for title in results.keys():
     dfResults, dfYield, dfCOP = results[title]   # Unpacking.
-    sh = wb.sheets.add(name=f'dfResults_{title}')
-    sh.range('A1').value = title
-    sh.range('A2').value = 'All results'
-    sh.range('A10').value = dfResults
+    score, intercept, coef, model = performPolyFit(degree=degree, df=dfResults, featureNames=['Tsource', 'Tdrain'])
+    print(f'{title}: score: {score}, coef: {coef}, intercept: {intercept}')
+    regrs[title] = (score, intercept, coef, model)
+    pass
 
-    sh = wb.sheets.add(name=f'dfYield_{title}')
-    sh.range('A1').value = title
-    sh.range('A2').value = 'Yield'
+
+# Write the results to Excel files.
+xlApp = xw.App(visible=True, add_book=False)
+wb = xlApp.books.open('HP performance metrics TEMPLATE.xlsx')
+
+for title in results.keys():
+    dfResults, dfYield, dfCOP = results[title]    # Unpacking thermodynamic results.
+    score, intercept, coef, model = regrs[title]  # Unpacking regression results.
+
+    sh = wb.sheets['Refrig'].copy(name=title)
+    pCond = dfResults['pCond'].iloc[0]
+    sh.range('B1').value = title[:3]
+    sh.range('B2').value = TsourceRefC
+    sh.range('B3').value = TdrainRefC
+    sh.range('B4').value = pCond / 1E5
+    sh.range('B5').value = degree
+    sh.range('B6').value = score
+    sh.range('B7').value = intercept  # Special handling of the intercept.
+    sh.range('C7').value = coef[1:]
+
     sh.range('A10').value = dfYield
+    sh.range('A30').value = dfCOP
+    sh.range('A50').value = dfResults
 
-    sh = wb.sheets.add(name=f'dfCOP_{title}')
-    sh.range('A1').value = title
-    sh.range('A2').value = 'Carnot COP'
-    sh.range('A10').value = dfCOP
 
-wb.save('HP performance metrics.xlsx')
+wb.save(f'HP performance metrics - {degree=}.xlsx')
 wb.close()
 xlApp.quit()
